f571b3bf46a20b1a4562e3e94572ff03240249eb
Rajesh Kumar <114206426+RajeshKumar-09@users.noreply.github.com>
2023-01-23 23:27:53 +0500

    Add files via upload

---
+++ Audio.java
@@ -0,0 +1,42 @@
+import java.awt.*;
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+import javax.sound.sampled.*;
+import javax.swing.*;
+public class Audio {
+
+    public static void main(String[] args) throws UnsupportedAudioFileException, IOException, LineUnavailableException{
+
+        Scanner scanner = new Scanner(System.in);
+        File file = new File("Little Fish - Quincas Moreira (1).wav");
+        //File file = new File("Level_Up.wav");
+        AudioInputStream audioStream = AudioSystem.getAudioInputStream(file);
+        Clip clip = AudioSystem.getClip();
+        clip.open(audioStream);
+
+        String response = "";
+
+
+        while(!response.equals("Q")) {
+            System.out.println("P = play, S = Stop, R = Reset, Q = Quit");
+            System.out.print("Enter your choice: ");
+            response = scanner.next();
+            response = response.toUpperCase();
+
+            switch(response) {
+                case ("P"): clip.start();
+                    break;
+                case ("S"): clip.stop();
+                    break;
+                case ("R"): clip.setMicrosecondPosition(0);
+                    break;
+                case ("Q"): clip.close();
+                    break;
+                default: System.out.println("Not a valid response");
+            }
+
+        }
+        System.out.println("Byeeee!");
+    }
+}
